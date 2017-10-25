e33600d999ca882f87789035e0d1ba03a6b73b03
Aparna Karve <akarve@redhat.com>
2017-10-25 07:59:01 -0700

    merge with commit2

--- app/presenters/tree_builder_generic_object_definition.rb
+++ app/presenters/tree_builder_generic_object_definition.rb
@@ -1,6 +1,6 @@
 class TreeBuilderGenericObjectDefinition < TreeBuilder
   has_kids_for GenericObjectDefinition, [:x_get_god_kids]
-  has_kids_for GenericObjectDefinition, [:x_get_actions_kids]
+  has_kids_for Hash, [:x_get_actions_kids]
 
   private
 
@@ -17,12 +17,12 @@ class TreeBuilderGenericObjectDefinition < TreeBuilder
 
   def x_get_god_kids(object, count_only)
     count = object.custom_button_sets.count + object.custom_buttons.count
-    objects = count > 0 ? [{:id => object.id.to_s, :text => _('Actions'), :icon => 'pficon pficon-folder-close', :tip => _('Actions')}] : []
+    objects = count > 0 ? [{:id => object.id.to_s, :text => _('Actions'), :icon => 'pficon pficon-folder-close', :tip => _('Actions'), :object => object}] : []
     count_only_or_objects(count_only, objects)
   end
 
   def x_get_actions_kids(object, count_only)
-    count_only_or_objects(count_only, object.custom_button_sets + object.custom_buttons, :name)
+    count_only_or_objects(count_only, object[:object].custom_button_sets + object[:object].custom_buttons, :name)
   end
 
   def tree_init_options(_tree_name)
